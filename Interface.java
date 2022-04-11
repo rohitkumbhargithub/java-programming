c34e91d7d3812d413c9d85ce81ae38257b1cd72c
rohitkumbhargithub <101877080+rohitkumbhargithub@users.noreply.github.com>
2022-04-11 22:37:52 +0530

    Update Interface.java

--- Interface.java
+++ Interface.java
@@ -1,20 +1,24 @@
 import java.util.*;
 interface student{
- void getdata();
- void displaydata();
+ void getdata();//interface method (does not have a body)
+ void displaydata();//interface method (does not have a body)
  }
  interface marksDetails{
-  void marks();
-  void displaymarks();
+  void marks();//interface method (does not have a body)
+  void displaymarks();//interface method (does not have a body)
   }
-  class course implements student, marksDetails{
+  class course implements student, marksDetails
+  //course 'implements' student and marksDetails interface
+{
    Scanner sc=new Scanner(System.in);
    Scanner sc1=new Scanner(System.in);
    int rollno;
    float per;
    int m1, m2, m3, m4, total;
    String name, cname, dob, mname;
-   public void getdata(){
+   public void getdata()
+     //The body of getdata() 
+  {
    System.out.println("Enter Student Roll no: ");
    rollno=sc.nextInt();
    System.out.println("Enter Student Full name: ");
@@ -27,14 +31,18 @@ interface student{
    System.out.println("Enter Mother name: ");
    mname=sc.nextLine();
    }
- public void displaydata(){
+ public void displaydata()
+  //The body of dusplaydata()
+{
    System.out.println("Roll no: " +rollno);
    System.out.println("Student name: " +name);
    System.out.println("Course name: " +cname);
    System.out.println("Brith date: " +dob);
    System.out.println("Mother name: " +mname);
    }
-  public void marks(){
+  public void marks()
+  //The body of marks() 
+{
    System.out.println("\n**ENTER MARK**\n");
     System.out.println("Enter Subject 1: ");
     m1=sc1.nextInt();
@@ -46,7 +54,9 @@ interface student{
     m4=sc1.nextInt();
     total=m1+m2+m3+m4;
     }
-    public void displaymarks(){
+    public void displaymarks()
+    //The body of displaymarks() 
+{
     System.out.println("out of 400 = " +total);
     per=total/4;
     System.out.println("Percentage is " +per);
@@ -74,7 +84,7 @@ interface student{
   }
      public class Main{
       public static void main(String args[]){
-        course cr= new course();
+        course cr= new course(); ////Creating an object for course.
         System.out.println("\n**Student information**\n");
         cr.getdata();
         cr.marks();
