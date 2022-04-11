0ea3ba6befa2c6a298da30e1ca8fe54258aeba18
rohitkumbhargithub <101877080+rohitkumbhargithub@users.noreply.github.com>
2022-04-11 22:27:54 +0530

    Update Interface.java

--- Interface.java
+++ Interface.java
@@ -1,80 +1,88 @@
-//interface in java
 import java.util.*;
 interface student{
-    void getstudentdata();//interface method (does not have a body)
-}
-interface academic{
-    void marks();//interface method (does not have a body)
-}
-class college implements student, academic
-/*College 'implements' student and acadmic interface*/
-{
-    Scanner sc=new Scanner(System.in);
-    public void getstudentdata()/*The body of getstudentdata() */
+ void getdata();
+ void displaydata();
+ }
+ interface marksDetails{
+  void marks();
+  void displaymarks();
+  }
+  class course implements student, marksDetails{
+   Scanner sc=new Scanner(System.in);
+   Scanner sc1=new Scanner(System.in);
+   int rollno;
+   float per;
+   int m1, m2, m3, m4, total;
+   String name, cname, dob, mname;
+   public void getdata(){
+   System.out.println("Enter Student Roll no: ");
+   rollno=sc.nextInt();
+   System.out.println("Enter Student Full name: ");
+   name=sc.nextLine();
+   name=sc.nextLine();
+   System.out.println("Enter Student Course name: ");
+   cname=sc.nextLine();
+   System.out.println("Enter Student Birth date: ");
+   dob=sc.nextLine();
+   System.out.println("Enter Mother name: ");
+   mname=sc.nextLine();
+   }
+ public void displaydata(){
+   System.out.println("Roll no: " +rollno);
+   System.out.println("Student name: " +name);
+   System.out.println("Course name: " +cname);
+   System.out.println("Brith date: " +dob);
+   System.out.println("Mother name: " +mname);
+   }
+  public void marks(){
+   System.out.println("\n**ENTER MARK**\n");
+    System.out.println("Enter Subject 1: ");
+    m1=sc1.nextInt();
+    System.out.println("Enter Subject 2: ");
+    m2=sc1.nextInt();
+    System.out.println("Enter Subject 3: ");
+    m3=sc1.nextInt();
+    System.out.println("Enter Subject 4: ");
+    m4=sc1.nextInt();
+    total=m1+m2+m3+m4;
+    }
+    public void displaymarks(){
+    System.out.println("out of 400 = " +total);
+    per=total/4;
+    System.out.println("Percentage is " +per);
+    if(m1<=35 || m2<=35 || m3<=35 || m4<=35)
     {
-        String name, cname, address,mob;
-        int roll_no;
-        System.out.println("Enter roll no: ");
-        roll_no=sc.nextInt();
-        System.out.println("Enter name: ");
-        name=sc.nextLine();
-        name=sc.nextLine();
-        System.out.println("Enter course name: ");
-        cname=sc.nextLine();
-        System.out.println("Enter your Address: ");
-        address=sc.nextLine();
-        System.out.println("Enter mobile no: ");
-        mob=sc.nextLine();
-        
-        //printing the get student data.
-        
-        System.out.println("\n***Student Information***\n");
-        System.out.println("Roll no :" +roll_no);
-        System.out.println("Name : " +name);
-        System.out.println("course name : " +cname);
-        System.out.println("Address : " +address);
-        System.out.println("Mobile no : " +mob);
-    }
-    public void marks()/*The body of marks() */{
-        Scanner sc1=new Scanner(System.in);
-        
-        //Inputing four subject mark.
-     
-        int m1, m2, m3, m4;
-        System.out.println("Enter subject first: ");
-        m1=sc1.nextInt();
-        System.out.println("Enter subject second: ");
-        m2=sc1.nextInt();
-        System.out.println("Enter subject third: ");
-        m3=sc1.nextInt();
-        System.out.println("Enter subject fourth: ");
-        m4=sc1.nextInt();
-        
-        //Printing marks.
-        
-        System.out.println("\n Inputed marks is \n Subject first "+m1+ "\nSubject second "+m2+ "\nSubject third "+m3+"\nsubject fourth "+m4);
-        
-        //Percentage for subjects.
-        
-        System.out.println("\n**Percenatge**\n");
-        float per;
-        per=(m1+m2+m3+m4)/4;
-        System.out.println("Percantage is " +per+"%");
-        
-        //less than 18 percentage to fail.
-        
-        if(per>18)
-         System.out.println("your are pass..");
-        else 
-         System.out.println("you are fail...");
-    }
+      System.out.println("You are fail..");
+      }
+     else if(per<=50)
+     {
+     System.out.println("You are pass..");
+     }
+     else if(per<=75)
+     {
+     System.out.println("Secnod class..");
+     }
+     else if(per<=100)
+     {
+     System.out.println("First class..");
+     }
+     else
+     {
+     System.out.println("Your input is wrong..");
+     }
+     }
+  }
+     public class Main{
+      public static void main(String args[]){
+        course cr= new course();
+        System.out.println("\n**Student information**\n");
+        cr.getdata();
+        cr.marks();
+        System.out.println("\n**Given information**\n");
+        cr.displaydata();
+        cr.displaymarks();
 }
-public class Main{
-    public static void main(String args[]){
-        college C=new college(); //Creating an object for college.
-        System.out.println("\n**Input Student information**\n");
-        C.getstudentdata();
-       System.out.println("\n**Enter marks**\n");
-        C.marks();
-    }
-}
+} 
+
+
+
